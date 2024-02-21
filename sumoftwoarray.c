839097052c23bdff73c7f6f1e3ce7d417131bd2f
Bharathkumar <144337794+Bharathkumar-2006@users.noreply.github.com>
2024-02-21 13:57:41 +0530

    updated code

---
+++ sumoftwoarray.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int n;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    int *arr1 = (int*)malloc(n*sizeof(int));
+    int *arr2 = (int*)malloc(n*sizeof(int));
+    int *arr3 = (int*)malloc(n*sizeof(int));
+
+    printf("Enter the elements of the first array:");
+    for(int i=0;i<n;i++){
+        scanf("%d",arr1+i);              
+    }
+
+    printf("\nEnter the elements of the second array:");
+    for(int i=0;i<n;i++){
+        scanf("%d",arr2+i);  
+        *(arr3+i)= *(arr1+i) + *(arr2+i);            
+    }
+    printf("The sum of two array elements:");
+    for(int i=0;i<n;i++){
+        printf(" %d\t",*(arr3+i));
+    }
+    return 0;
+    
+}
