99281465085f996abe9451fdc83b817e929a354b
Bharathkumar <144337794+Bharathkumar-2006@users.noreply.github.com>
2024-02-21 13:24:45 +0530

    updated code

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(){
+    int sum=0;
+    int *arr = (int*)malloc(5*sizeof(int));
+
+    printf("Enter the elements of the array:");
+
+    for(int i=0;i<5;i++){
+        scanf("%d",arr+i);
+    }
+    for(int i=0;i<5;i++){
+        printf("%d\t",*(arr+i));
+    }
+    return 0;
+
+}
