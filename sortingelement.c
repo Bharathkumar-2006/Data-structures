59ee5b3d15eadc1404fb609e6f335bf1781e4025
Bharathkumar <144337794+Bharathkumar-2006@users.noreply.github.com>
2024-02-21 15:57:50 +0530

    updated

--- sortingelement.c
+++ sortingelement.c
@@ -14,13 +14,16 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",arr+i);        
     }
-    for(int i=0;i<n;i++){
-        if(*(arr+i) > *(arr+i+1)){
-            int temp = *(arr+i);
-            (arr+i) = *(arr+i+1);
-            arr+i+1 = temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (*(arr + j) > *(arr + j + 1)) {
+                int temp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
         }
     }
+    
     printf("\nThe sorted array is:");
     for(int i=0;i<n;i++){
         printf("%d\t",*(arr+i));
